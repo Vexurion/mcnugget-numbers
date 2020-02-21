392a9532ce350fae5308a1fc8958b279215e996a
Alexx <Vexurion@gmail.com>
2020-02-21 12:03:44 -0700

    Homework written.

--- src/edu/cnm/deepdive/Frobenius.java
+++ src/edu/cnm/deepdive/Frobenius.java
@@ -16,4 +16,16 @@ public class Frobenius {
 
   }
 
+  /**
+   * return true if the stated value cna be formed as a sum of non-negative integral multiples of
+   * the elements of packSizes.
+   *
+   * @param value target/goal number.
+   * @param packSizes array of distinct pack sizes, in descending order.
+   * @return true if value is a McNugget number using the specific pack sizes, false otherwise.
+   */
+  public static boolean isGeneralMcNugget(int value, int[] packSizes) {
+    return false; //TODO Complete implementation for extra credit.
+  }
+
 }
