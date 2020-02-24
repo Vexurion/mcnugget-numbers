900cdf0e3cab964656554db7914215c6a511cf10
Alexx <Vexurion@gmail.com>
2020-02-23 23:07:26 -0700

    Attempt

--- src/edu/cnm/deepdive/Frobenius.java
+++ src/edu/cnm/deepdive/Frobenius.java
@@ -17,15 +17,34 @@ public class Frobenius {
   }
 
   /**
-   * return true if the stated value cna be formed as a sum of non-negative integral multiples of
-   * the elements of packSizes.
+   * Determines if {@code value} can be expressed as the sum of non-negative integral multiples of
+   * the elements of {@code packSizes}. (If we invoke {@code
+   * isGeneralMcNugget(value, new int[]{20, 9, 6})}, the result returned should be identical to that
+   * returned by {@link #isMcNugget(int) isMcNugget(value)}.
    *
    * @param value target/goal number.
-   * @param packSizes array of distinct pack sizes, in descending order.
-   * @return true if value is a McNugget number using the specific pack sizes, false otherwise.
+   * @param packSizes array of distinct, positive {@code int }pack sizes, in descending order.
+   * @return {@code true} if {@code value} is a McNugget number using the specific pack sizes,
+   * {@code false} otherwise.
    */
   public static boolean isGeneralMcNugget(int value, int[] packSizes) {
-    return false; //TODO Complete implementation for extra credit.
+    boolean flag = false;
+    if (packSizes.length == 1 || packSizes.length == 0) {
+      if (value >= 0) {
+        if (value != 0) {
+          isGeneralMcNugget(value - packSizes[0], packSizes);
+        }
+      }
+      flag = true;
+    }
+    for (int packSize : packSizes) {
+      int[] ithElementArray = {packSize};
+      isGeneralMcNugget(value, ithElementArray);
+    }
+    return flag; // TODO Complete implementation for extra credit.
+
   }
 
+
 }
+
--- tests/edu/cnm/deepdive/FrobeniusTest.java
+++ tests/edu/cnm/deepdive/FrobeniusTest.java
@@ -7,10 +7,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 class FrobeniusTest {
+  int[] inputArray = {20, 9, 6};
+  int value = 29;
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "general-mcnugget-data.csv")
+  void isGeneralMcNugget(int value, int[] packSizes) {
+    assertEquals(false, Frobenius.isGeneralMcNugget(value, inputArray));
+  }
 
   @ParameterizedTest
   @CsvFileSource(resources = "mcnugget-data.csv", numLinesToSkip = 1)
   void isMcNugget(int value, boolean expected) {
     assertEquals(expected, Frobenius.isMcNugget(value));
   }
+
+
+
 }
